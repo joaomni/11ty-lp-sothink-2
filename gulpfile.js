fc59c719b09061e2c04c89a2ed88544cb5d95c84
puppycat710 <puppycat710@proton.me>
2025-05-26 17:13:15 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,25 @@ const { src, dest, series } = require('gulp')
 const uglify = require('gulp-uglify')
 const minifyHtml = require('gulp-minify-html')
 const cssnano = require('gulp-cssnano')
-
 // Minify HTML
 const buildHTML = () => {
 	return src('./*.html') // Caminho ajustado
 		.pipe(minifyHtml())
 		.pipe(dest('./')) // Sobrescreve os arquivos originais
 }
-
 // CSSNano
 const buildCSS = () => {
 	return src('./css/*.css').pipe(cssnano()).pipe(dest('./css/'))
 }
-/** 
 // Babel
-const buildBabel = () => {
-  return src('./js/*.js')
-    .pipe(babel())
-    .pipe(dest('./js/'));
-};
-**/
+// const buildBabel = () => {
+//   return src('./js/*.js')
+//     .pipe(babel())
+//     .pipe(dest('./js/'));
+// };
 // Uglify
-const buildJS = () => {
-	return src('./js/*.js').pipe(uglify()).pipe(dest('./js/'))
-}
+// const buildJS = () => {
+// 	return src('./js/*.js').pipe(uglify()).pipe(dest('./js/'))
+// }
 
-exports.buildDev = series(buildHTML, buildCSS, buildJS)
+exports.buildDev = series(buildHTML, buildCSS)
